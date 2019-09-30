011a1704b8e6f426161ea1d07e07473ed5126157
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-09-30 14:22:40 +0100

    chore(setup): setup the environment
    - setup webpack to build react app
    - setup babel
    - setup eslint with airbnb style guide
    - setup folder structure
    
    [Start #168823020]

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const title = 'Welcome to Sound-wave'
+const title = 'Welcome to Sound-wave';
 ReactDOM.render(<App title={title} />, document.getElementById('root'));
 
 module.hot.accept();
